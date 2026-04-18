d74aec8a29b13016ef0376c435cdffa27761611d
Kirill <kyrill.dodulak@gmail.com>
2026-04-18 10:28:39 +0300

    fix: deleted StyleSheet

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { useRouter } from 'expo-router';
 import { HomeStyles as styles } from '../src/styles/HomeStyles';
 
